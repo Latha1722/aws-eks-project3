5e2cb031688f79fc7a53d64d7a922cc1ac6c99a0
Latha1722 <latha_paladugu@blr.trigent.com>
2026-03-03 15:50:30 +0530

    first

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -15,6 +15,17 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 
+# Required for ALB to find subnets
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
   tags = {
     Environment = "dev"
     Project     = "mern-microservices"
@@ -36,12 +47,36 @@ module "eks" {
 
   enable_irsa = true
 
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_private_access      = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+   # ✅ ADD THIS BLOCK
+  access_entries = {
+    github_actions = {
+      principal_arn = "arn:aws:iam::466825169909:user/latha"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.small"]
       min_size       = 1
       max_size       = 3
       desired_size   = 2
+
+      iam_role_additional_policies = {
+        ecr_read = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      }
     }
   }
 
@@ -49,4 +84,45 @@ module "eks" {
     Environment = "dev"
     Project     = "mern-microservices"
   }
+}
+
+# =========================
+# AWS Load Balancer Controller
+# =========================
+module "aws_load_balancer_controller_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.3.1"
+
+  role_name = "aws-load-balancer-controller"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+# Extra policy to allow AddTags
+resource "aws_iam_role_policy" "load_balancer_extra" {
+  name = "load-balancer-extra-policy"
+  role = module.aws_load_balancer_controller_irsa_role.iam_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["*"]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:RequestedRegion" = "ap-south-1"
+          }
+        }
+      }
+    ]
+  })
 }
